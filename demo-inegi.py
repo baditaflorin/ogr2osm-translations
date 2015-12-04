c25f73558f7debf05a734fc0b7d9744b8dcd271b
Martijn van Exel <martijnv@telenav.com>
2015-12-04 12:56:32 -0700

    adding demo translation for INEGI IVLA file for Cygnus demo

---
+++ demo-inegi.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+A simplified translation function for INEGI 'Información Vectorial de Localidades Amanzanadas y Números Exteriores' data
+
+DO NOT USE. This is only to demonstrate the capabilities of Cygnus with a simple example.
+'''
+
+def filterTags(attrs):
+    if not attrs:
+        return
+    tags = {}
+
+    if 'TIPOVIAL'in attrs:
+        source_type = attrs.get('TIPOVIAL')
+        if source_type == 'CALLE':
+            tags['highway'] = 'residential'
+        if source_type == 'PRIVADA':
+            tags['highway'] = 'service'
+            tags['access'] = 'private'
+
+    if 'NOMVIAL' in attrs:
+        source_name = attrs.get('NOMVIAL')
+        if source_name != 'NINGUNO':
+            tags['name'] = '{} {}'.format(
+                attrs.get('TIPOVIAL'),
+                source_name).title()
+
+    tags['source'] = 'INEGI'
+
+    return tags
